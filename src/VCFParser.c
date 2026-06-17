52caec10695572afb57977caa773989680b9bc03
TQ-Smith <thomasqsmith2000@gmail.com>
2026-06-16 20:34:04 -0400

    Working on VCF Parser ...

---
+++ src/VCFParser.c
@@ -0,0 +1,184 @@
+
+// File: VCFParser.c
+// Date: 13 May 2025
+// Author: T. Quinn Smith
+// Principal Investigator: Dr. Zachary A. Szpiech
+// Purpose: Parse VCF files. Adapted from EGGS
+
+#include "VCFParser.h"
+
+InputStream_t* init_input_stream(char* source) {
+    InputStream_t* inputStream = (InputStream_t*) calloc(1, sizeof(InputStream_t));
+    inputStream -> file = gzopen(source, "r");
+    inputStream -> fpIn = ks_init(inputStream -> file);
+    inputStream -> buffer = (kstring_t*) calloc(1, sizeof(kstring_t));
+    return inputStream;
+}
+
+void destroy_input_stream(InputStream_t* inputStream) {
+    if (inputStream == NULL)
+        return;
+    if (inputStream -> fpIn != NULL)
+        ks_destroy(inputStream -> fpIn);
+    if (inputStream -> buffer != NULL) {
+        if (inputStream -> buffer -> s != NULL)
+            free(inputStream -> buffer -> s);
+        free(inputStream -> buffer);
+    }
+    gzclose(inputStream -> file);
+    free(inputStream);
+}
+
+VCF_t* init_vcf(InputStream_t* inputStream, bool eat) {
+    if (inputStream == NULL)
+        return NULL;
+
+    // Allocate our VCF_t.
+    VCF_t* vcf = (VCF_t*) calloc(1, sizeof(VCF_t));
+
+    // If we have header lines to eat.
+    if (eat) {
+        int dret;
+        do {
+            ks_getuntil(inputStream -> fpIn, '\n', inputStream -> buffer, &dret);
+        } while (strncmp(inputStream -> buffer -> s, "#C", 2) != 0);
+    }
+
+    // Count the number of samples in the VCF file.
+    int numSamples = 0;
+    for (int i = 0; i < inputStream -> buffer -> l; i++)
+        if (inputStream -> buffer -> s[i] == '\t')
+            numSamples++;
+    vcf -> numSamples = numSamples - 8;
+    // Copy the sample names from the header line.
+    vcf -> sampleNames = calloc(vcf -> numSamples, sizeof(char*));
+    // Read in the sample names.
+    char* header = strdup(inputStream -> buffer -> s);
+    char* tok = NULL;
+    tok = strtok(header, "\t");
+    for (int i = 0; i < 9; i++)
+        tok = strtok(NULL, "\t");
+    for (int i = 0; i < vcf -> numSamples; i++) {
+        vcf -> sampleNames[i] = strdup(tok);
+        tok = strtok(NULL, "\t");
+    }
+    
+    return vcf;
+}
+
+bool get_next_vcf_record(Record_t* record, InputStream_t* inputStream) {
+
+    int dret = 0, numTabs = 0, prevIndex = 0, numAlleles = 2;
+
+    // Get the next line.
+    ks_getuntil(inputStream -> fpIn, '\n', inputStream -> buffer, &dret);
+
+    // If end of file, return false.
+    if (ks_eof(inputStream -> fpIn) || inputStream -> buffer -> l == 0)
+        return false;
+
+    // This is alittle clunky, but I think it is faster than splitting on '\t'.
+    for (int i = 0; i <= inputStream -> buffer -> l; i++) {
+        // If end of the line is reached or a tab was encountered.
+        if (i == inputStream -> buffer -> l || inputStream -> buffer -> s[i] == '\t') {
+            if (numTabs == 0) {
+                // The first field in a record is the chromosome name.
+                if (record -> chrom != NULL)
+                    free(record -> chrom);
+                record -> chrom = strndup(inputStream -> buffer -> s, i);
+            } else if (numTabs == 1) {
+                // The second field is the position on the chromosome.
+                record -> position = (int) strtol(inputStream -> buffer -> s + prevIndex + 1, (char**) NULL, 10);
+            } else if (numTabs == 4) {
+                // The fifth field holds the ALT alleles. Each record has at least two alleles.
+                //  Each additional allele is appended withinputStream -> buffer -> s a ','. For each ',' encountered,
+                //  increment the number of alleles in the record.
+                for (int j = prevIndex + 1; inputStream -> buffer -> s[j] != '\t'; j++)
+                    if (inputStream -> buffer -> s[j] == ',')
+                        numAlleles++;
+            } else if (numTabs > 8) {
+                Genotype_t temp;
+                temp.left = MISSING;
+                temp.right = MISSING;
+                temp.isPhased = false;
+                char* start = inputStream -> buffer -> s + prevIndex + 1;
+                char* next = start;
+                if (start[0] != '.')
+                    temp.left = (int) strtol(start, &next, 10);
+                if (next[0] == '|') 
+                    temp.isPhased = true;
+                if ((next[0] == '|' || next[0] == '/') && next[1] != '.')
+                    temp.right = (int) strtol(next + 1, (char**) NULL, 10);
+                
+                int sampleIndex = numTabs - 9;
+                record -> genotypes[sampleIndex].left = temp.left;
+                record -> genotypes[sampleIndex].right = temp.right;
+                record -> genotypes[sampleIndex].isPhased = temp.isPhased;
+            }
+            prevIndex = i;
+            numTabs++;
+        }
+    }
+    record -> numAlleles = numAlleles;
+
+    // Successfully parsed record.
+    return true;
+
+}
+
+void parse_vcf(VCF_t* vcf, InputStream_t* inputStream, bool keep, bool hap) {
+    int recordIndex = 0;
+    while (true) {
+
+        // Allocate memory for a new record.
+        Record_t* record = (Record_t*) calloc(1, sizeof(Record_t));
+        record -> genotypes = (Genotype_t*) calloc(vcf -> numSamples, sizeof(Genotype_t));
+        record -> numSamples = vcf -> numSamples;
+        record -> recordIndex = recordIndex;
+
+        // While not EOF, add record to the list.
+        if (get_next_vcf_record(record, inputStream)) {
+            if (vcf -> numRecords == 0) {
+                vcf -> headRecord = record;
+                vcf -> tailRecord = record;
+            } else {
+                vcf -> tailRecord -> nextRecord = record;
+                vcf -> tailRecord = record;
+            }
+            vcf -> numRecords++;
+        } else {
+            destroy_record(record);
+            break;
+        }
+        recordIndex++;
+    }
+
+}
+
+void destroy_record(Record_t* record) {
+    if (record == NULL)
+        return;
+    if (record -> genotypes != NULL)
+        free(record -> genotypes);
+    if (record -> chrom != NULL)
+        free(record -> chrom);
+    free(record);
+}
+
+void destroy_vcf(VCF_t* vcf) {
+    if (vcf == NULL)
+        return;
+    Record_t* temp = NULL;
+    for (int i = 0; i < vcf -> numRecords; i++) {
+        temp = vcf -> headRecord;
+        vcf -> headRecord = vcf -> headRecord -> nextRecord;
+        destroy_record(temp);
+    }
+    if (vcf -> sampleNames != NULL) {
+        for (int i = 0; i < vcf -> numSamples; i++) {
+            free(vcf -> sampleNames[i]);       
+        }
+        free(vcf -> sampleNames);
+    }
+    free(vcf);
+}
---
+++ src/VCFParser.h
@@ -0,0 +1,112 @@
+
+// File: VCFParser.h
+// Date: 13 May 2025
+// Author: T. Quinn Smith
+// Principal Investigator: Dr. Zachary A. Szpiech
+// Purpose: Adapted from EGGS.
+
+#ifndef VCF_PARSER_H
+#define VCF_PARSER_H
+
+#include <stdbool.h>
+#include "kseq.h"
+#include <zlib.h>
+#include <stdio.h>
+
+// We use -1 to denote a missing genotype.
+#define MISSING -1
+
+// A stream to read GZIP files.
+#define BUFFER_SIZE 4096
+KSTREAM_INIT(gzFile, gzread, BUFFER_SIZE)
+
+// Represents the genotype of a sample at a locus.
+typedef struct {
+    // If the genotype is phased.
+    bool isPhased;
+    // The left and right genotypes.
+    int left;
+    int right;
+} Genotype_t;
+
+// Represents a record/locus.
+typedef struct Record {
+    // The chromosome.
+    char* chrom;
+    // The position on the chromosome.
+    int position;
+    // The total number of alleles at the locus.
+    int numAlleles;
+    // Redunant but useful. The number of samples in the genotypes array.
+    int numSamples;
+    // The index of the record in the list.
+    int recordIndex;
+    // The genotypes for each sample.
+    Genotype_t* genotypes;
+    // A pointer to the next locus. Record_t is a node in a linked list.
+    struct Record* nextRecord;
+} Record_t;
+
+
+// A VCF is a linked list of records.
+typedef struct {
+    int numSamples;
+    // Number of records in the list.
+    int numRecords;
+    // The sample names.
+    char** sampleNames;
+    // Head and tail pointers.
+    Record_t* headRecord;
+    Record_t* tailRecord;
+} VCF_t;
+
+// Packages all the necessary information to read from a GZIP file.
+typedef struct {
+    // The file we are reading.
+    gzFile file;
+    // The stream that wraps the file.
+    kstream_t* fpIn;
+    // The buffer to read in lines of the file.
+    kstring_t* buffer;
+} InputStream_t;
+
+// From a file name, open up a GZIP stream.
+// Accepts:
+//  char* source -> The file name open. If null, read from stdin.
+// Returns: InputStream_t*, the input stream we are reading from. NULL, if source does not exist. 
+InputStream_t* init_input_stream(char* source);
+
+// Closes file stream and deallocates memory.
+void destroy_input_stream(InputStream_t* inputStream);
+
+// From an input stream reading a VCF file, create an empty VCF.
+// Accepts:
+//  InputStream_t* inputStream -> The input stream to read from.
+//  bool eat -> If we should eat the header lines.
+// Returns: VCF_t*, an empty replicate with sampleNames set from VCF header info.
+VCF_t* init_vcf(InputStream_t* inputStream, bool eat);
+
+// Read in next VCF record from input stream.
+// Accepts:
+//  Record_t* record -> An allocated record. Set fields.
+//  InputStream_t* inputStream -> The VCF file we are reading.
+// Returns: bool, true is a record was read. false, if eof.
+bool get_next_vcf_record(Record_t* record, InputStream_t* inputStream);
+
+// Reads in whole VCF file and adds each record to VCF.
+// Accepts:
+//  VCF_t* vcf -> An allocated, but empty, vcf to add records to.
+//  InputStream_t* inputStream -> The VCF file we are reading from.
+//  bool dropMonomorphicSites -> Drop sites if monomorphic.
+//  double maf -> Minor allele frequency threshold.
+//  double afMissing -> Missing allele frequency threshold.
+// Returns: void.
+void parse_vcf(VCF_t* vcf, InputStream_t* inputStream, bool dropMonomorphicSites, double maf, double afMissing);
+
+// Frees memory associated with a record.
+void destroy_record(Record_t* record);
+
+// Frees memory associated with a VCF.
+void destroy_vcf(VCF_t* vcf);
+
+#endif
