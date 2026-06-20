1b79cfad65fdef3b4d7a209c2b49d34441fe3d32
TQ-Smith <thomasqsmith2000@gmail.com>
2026-06-20 12:25:23 -0400

    Working on iHS calculations ...

---
+++ src/BlockList.c
@@ -0,0 +1,22 @@
+
+#include "BlockList.h"
+
+BlockList_t* init_block_list(bool phased, bool fuzzy) {
+
+}
+
+Block_t* init_block(char* chrom, int startPosition, int endPosition) {
+
+}
+
+void append_block(BlockList_t* blockList, Block_t* block) {
+
+}
+
+void destroy_block(BlockList_t* block) {
+
+}
+
+void destroy_stats(BlockList_t* blockList) {
+    
+}
